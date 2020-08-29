49f77f4110ebb25b33ff46db41179194355cffb8
FrancoisMartinage <francois@app-advisory.com>
2020-08-29 10:50:08 +0200

    Basic gameplay completion, everything is working correctly

--- Assets/___EXEMPLEGAME_Change this name/Scripts/Enemies/EnemyControllerBonefire.cs
+++ Assets/___EXEMPLEGAME_Change this name/Scripts/Enemies/EnemyControllerBonefire.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections;
+using System.Security.Cryptography;
+using FrancoisSauce.Scripts.FSSingleton;
 using FrancoisSauce.Scripts.FSUtils.FSGlobalVariables;
 using UnityEngine;
 
@@ -12,14 +14,35 @@ public class EnemyControllerBonefire : MonoBehaviour
     [SerializeField] private FSGlobalFloatSO dashCooldown = null;
 
     [SerializeField] private Rigidbody myRigidbody = null;
+    [SerializeField] private Animator myAnimator = null;
     private Transform myTransform;
 
     [SerializeField] private Transform playerTransform = null;
+    private static readonly int Dashing = Animator.StringToHash("Dashing");
+    private static readonly int Victory = Animator.StringToHash("Victory");
 
     private void Awake()
     {
         myTransform = transform;
     }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        switch (other.gameObject.layer)
+        {
+            //Player layer
+            case 8:
+                myAnimator.SetBool(Victory, true);
+                GameLogic.Instance.OnLose();
+                break;
+            //BreakableWall layer
+            case 10:
+                Destroy(other.gameObject);
+                break;
+        }
+    }
+
+    //TODO enemy always look the direction he's moving
 
     public void OnUpdateGameStarted()
     {
@@ -27,9 +50,8 @@ public class EnemyControllerBonefire : MonoBehaviour
         
         if (isDashing)
         {
-            print(myRigidbody.velocity.magnitude);
             if (myRigidbody.velocity.magnitude > .01f) return;
-            print("Stop dash");
+            myAnimator.SetBool(Dashing, false);
             isDashing = false;
             dashTimer = 0f;
             return;
@@ -46,18 +68,20 @@ public class EnemyControllerBonefire : MonoBehaviour
         var lookPos = playerTransform.position - myTransform.position;
         lookPos.y = 0;
         var rotation = Quaternion.LookRotation(lookPos);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime);
-        myTransform.LookAt(playerTransform);
+        myTransform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 10);
     }
 
     private IEnumerator Dash()
     {
+        myAnimator.SetBool(Dashing, true);
         isDashing = true;
         isBlocked = true;
         
-        yield return new WaitForSecondsRealtime(.5f);
+        yield return new WaitForSecondsRealtime(2f);
 
-        isBlocked = false;
         myRigidbody.AddForce(myTransform.forward * 1000);
+        
+        yield return new WaitForSecondsRealtime(.5f);
+        isBlocked = false;
     }
 }
